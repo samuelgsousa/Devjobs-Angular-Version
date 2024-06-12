cd05ca0bbba55cfadb76096c180a444e3e472b8a
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-06-12 18:25:47 -0300

    .

--- Devjobs/src/main.ts
+++ Devjobs/src/main.ts
@@ -7,3 +7,7 @@ import { provideRouter } from '@angular/router';
 import routeConfig from './app/routes'
 
 
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+})
+  .catch((err) => console.error(err));
