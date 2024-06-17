6efa8bb062885aa2a14a4aea1296e76fafdf06ac
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-06-17 18:08:07 -0300

    arquivos deletados

--- Devjobs/docs/browser/chunk-LHZRYWK2.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./chunk-YHOLSLLF.js";var p=e((s,t)=>{t.exports=XMLHttpRequest});export default p();
--- Devjobs/docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-JKU2CVJS.mjs";import"./chunk-RWG6324C.mjs";import"./chunk-OP2YIVM7.mjs";import"./chunk-HHWZB7PA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
