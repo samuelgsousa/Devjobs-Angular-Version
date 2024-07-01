94398f53892c73fdf82e123d5f41fa39ee8184fb
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-07-01 01:31:40 -0300

    Downgrade do Typescript

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-4MACDDSB.mjs";import"./chunk-L5DUIONQ.mjs";import"./chunk-44X6ELFP.mjs";export{a as default};
--- docs/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a as o,b as r,c as n}from"./chunk-A64ROPDQ.mjs";import{Da as e}from"./chunk-L5DUIONQ.mjs";import"./chunk-44X6ELFP.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
